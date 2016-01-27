9fe5ef024c746897d1dc8db1b2438af3ce699759
kingstakh <kingstakh@yandex.ru>
2016-01-27 16:45:32 +0300

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -26,8 +26,8 @@
          var options = {
              replaceLineBreaks: false, // true to replace \n by a new line, false by default
              android: {
-                 intent: 'INTENT'  // send SMS with the native android SMS messaging
-                 //intent: '' // send SMS without open any other app
+                 //intent: 'INTENT'  // send SMS with the native android SMS messaging
+                 intent: '' // send SMS without open any other app
              }
          };
 
